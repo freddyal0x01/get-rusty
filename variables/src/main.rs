337cac149a3e21dc135a69dfbbded26f2b43c82c
falcanta <falcanta@akamai.com>
2025-02-04 12:09:21 -0500

    Added variables, types for rust

---
+++ variables/src/main.rs
@@ -0,0 +1,64 @@
+use core::num;
+
+fn my_function(x: i32, y: i32) -> i32 {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    let sum = x + y;
+    return sum;
+}
+
+fn main() {
+    // let mut x = 5;
+    // println!("The value of x is: {}", x);
+    // let x: &str = "six";
+    // println!("The value of x is: {}", x);
+
+    // const SUBSCRIBER_COUNT: u32 = 100_000;
+
+    // let sum = my_function(11, 22);
+    // println!("The sum is: {}", sum);
+
+    // let number = 150;
+
+    // if number < 10 {
+    //     println!("first condition was true")
+    // } else if number < 22 {
+    //     println!("second condition was true")
+    // } else {
+    //     println!("Catch all")
+    // }
+
+    // let mut counter = 0;
+
+    // let result = loop {
+    //     counter += 1;
+
+    //     if counter == 10 {
+    //         break counter;
+    //     }
+    // };
+
+    // println!("The result is {}", result);
+
+    // let mut number = 3;
+
+    // while number != 0 {
+    //     println!("{}!", number);
+
+    //     number -= 1;
+    // }
+
+    // println!("LIFTOFF!!!")
+
+
+    // let a = [10, 20, 30, 40, 50];
+
+    // for i in a.iter() {
+    //     println!("the value is: {}", i);
+    // }
+
+    // for number in 1..4 {
+    //     println!("{}!", number)
+    // }
+
+}
