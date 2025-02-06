447ed97a1ad8e5041d07f8459d16291dd64979a2
falcanta <falcanta@akamai.com>
2025-02-06 17:26:11 -0500

    added collection files

--- collections/src/main.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // Strings are stored as a collection of UTF-8 encoded bytes.
-
-    let s1 = String::from("Hello, ");
-    let s2 = String::from("world!");
-    let s3 = s1  + &s2;
-
-    println!("{}{}", s3, s2)
-}
