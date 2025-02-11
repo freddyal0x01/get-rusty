04e35068afcf67014ebeec8302a49d821c67c0cd
falcanta <falcanta@akamai.com>
2025-02-11 14:09:27 -0500

    added procedural macro files

--- adv_features/macros/procedural_macros/src/main.rs
+++ adv_features/macros/procedural_macros/src/main.rs
@@ -1,3 +1,33 @@
+use hello_macro::HelloMacro;
+use hello_macro_derive::HelloMacro;
+
+#[derive(HelloMacro)]
+struct Pancakes;
+
 fn main() {
-    println!("Hello, world!");
+    Pancakes::hello_macro();
+}
+
+// Attribute-like macros
+
+#[route(GET, "/")]
+fn index() {
+    //
+}
+
+#[proc_macro_attribute]
+pub fn route(
+    attr: TokenStream, // GET, "/" - Contents of the attribute
+    item: TokenStream // fn index() { ... } - The body of the item that the attribute is attached to
+) -> TokenStream {
+    // ...
+}
+
+// Function-like macros
+
+let sql = sql!(SELECT * FROM posts WHERE id=1);
+
+#[proc_macro]
+pub fn sql(input: TokenStream) -> TokenStream {
+    // ...
 }
